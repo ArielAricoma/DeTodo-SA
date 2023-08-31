d08c3680f07f614be7e50f6a12e3e773ffdb4b04
ArielAricoma <141962108+ArielAricoma@users.noreply.github.com>
2023-08-31 12:24:19 -0300

    Probando

---
+++ src/detodo/sa/DeTodoSA.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package detodo.sa;
+
+/**
+ *
+ * @author ARIEL
+ */
+public class DeTodoSA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hola como estas");
+    }
+    
+}
